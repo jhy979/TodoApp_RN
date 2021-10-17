eff0cb65f760578bad23cfca90b0398263de425c
jhy979 <jhy979@gmail.com>
2021-10-17 19:46:53 +0900

    Textbox 생성

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { theme } from './colors';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.tc }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.tc : "white" }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
+      </View>
 
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder={working ? "무엇을 할까요?" : "어디로 떠날까요?"}
+          placeholderTextColor={theme.tc}
+          onChange={onChangeText}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,15 +52,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
   },
-  header:{
-    flexDirection:"row",
-    marginTop:100,
-    justifyContent:"space-between",
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
   },
-  btnText:{
-    fontSize:35,
-    color:theme.tc,
-  }
+  btnText: {
+    fontSize: 35,
+  },
+  input: {
+    backgroundColor: theme.toggle,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+    color: theme.tc,
+  },
 });
